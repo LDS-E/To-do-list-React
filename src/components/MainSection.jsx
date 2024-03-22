49a0dbb5f1c8215bf81510c5bb22ef954b6f0ef1
Lucas Eifler <lucass.eifler@gmail.comm>
2024-03-22 11:01:17 +0100

    adding svgs to assets

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import TaskItem from './TaskItem';
 import EditTaskContainer from './EditTaskContainer';
 import TaskSection from './TaskSection';
