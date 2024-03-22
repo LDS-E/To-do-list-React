9fdf95c386cde96a72f8035ac64fa33748e35271
AdriannaB79 <159770488+AdriannaB79@users.noreply.github.com>
2024-03-22 13:07:58 +0100

    header and Navbar code as components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Header from "./components/header";
 
 
 function App() {
 
   return (
-    <>
-    
-    </>
-  )
+    <div className="flex p-4">
+      <NavBar />
+      <Header />
+      {/* The rest of your app goes here */}
+    </div>
+  );
 }
 
 export default App
